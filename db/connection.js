3ded338cd714dffc677e698057f22644fbe7a195
Joey Deras <jmderas@yahoo.com>
2019-03-18 15:09:25 -0400

    added connection.js route

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,11 @@
 //This is the DB connection it will include how our database and Express.js will be connected using Mongoose.
+const mongoose = require("../models/Post");
+mongoose.Promise = Promise;
+const mongoUri = "mongodb://localhost/Breaddit";
+
+mongoose
+  .connect(mongoUri)
+  .then(connection => console.log(`Connection Established to db`))
+  .catch(connectionError => console.log("Connection Failed!", connectionError));
+
+module.exports = mongoose;
