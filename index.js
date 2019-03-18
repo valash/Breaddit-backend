88125fc0fc94ab759edf3c93ee9d48a178fe5a0d
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-03-18 10:22:51 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const parser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(parser.json());
+
+const postController = require('./controllers/posts');
+app.use('/breaddit/', postController);
+
+app.set('port', process.env.PORT || 3001);
+
+app.listen(app.get('port'));
