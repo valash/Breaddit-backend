5d0f6d62b215bcf799ce3f8230e2e5bf86250036
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-03-18 15:14:06 -0400

    read the seed data

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,15 @@
 //This is the controller for the post. It will most likely also include the commands for the comments since the comments will be a sub model. It' going to be using router b/c we have to do that, it'll be connected to the Post model and the connection file, so it can, you know access the dadtabase and actually use the model at hand, since those things are kinda important. Then it'll have a whole bunch of routes to places so it can CRUD it up. Bars.
 
 const express = require('express');
+const router = express.Router();
+
+const mongoose = require('../db/connection');
+const Post = mongoose.model('Post');
+
+router.get('/', (req, res) => {
+	Post.find({}).then((posts) => res.json(posts));
+});
+
+router.get('/:_id', (req, res) => {
+	Post.findOne({ _id: req.params._id }).then((post) => res.json(post));
+});
