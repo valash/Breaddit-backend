c1f4c9704610e6956bc9c6cd3e7bcbd1cdd02009
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-03-18 11:22:21 -0400

    This is so Joey can access this branch

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,3 @@
+//This is the controller for the post. It will most likely also include the commands for the comments since the comments will be a sub model. It' going to be using router b/c we have to do that, it'll be connected to the Post model and the connection file, so it can, you know
+
 const express = require('express');
---
+++ models/Post.js
@@ -0,0 +1,1 @@
+//This is the main model for the Breaddit clone. It will have full CRUD, but its only components are going to be a title and a body. KISS for the win.
