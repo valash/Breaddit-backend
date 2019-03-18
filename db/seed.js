f4fd318d630ad5dddaa0cc4d4e343a4f22c8a998
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-03-18 14:23:10 -0400

    initial seed.js and seedData

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,16 @@
 //this is going to be seed data for the database so that we as a team can make sure certain requests work across the board
+
+const mongoose = require('./connection');
+const seeds = require('./seedData');
+
+const Post = mongoose.model('Post');
+
+mongoose.Promise = Promise;
+
+Post.remove({}).then(() => {
+	console.log('DB has been emptied');
+	Post.collection.insert(seeds).then((seededEntries) => {
+		console.log(seededEntries);
+		mongoose.connection.close();
+	});
+});
