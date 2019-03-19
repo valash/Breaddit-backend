3a3aec7f747926b2c2a3d1e473d8f1f20931a252
Joey Deras <jmderas@yahoo.com>
2019-03-19 16:12:58 -0400

    config is good

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,18 @@
 //This is the DB connection it will include how our database and Express.js will be connected using Mongoose.
 const mongoose = require("../models/Post");
-mongoose.Promise = Promise;
-const mongoUri = "mongodb://localhost/Breaddit";
+// const mongoUri = "mongodb://localhost/Breaddit";
 
-mongoose
-  .connect(mongoUri)
-  .then(connection => console.log(`Connection Established to db`))
-  .catch(connectionError => console.log("Connection Failed!", connectionError));
+// mongoose
+//   .connect(mongoUri)
+//   .then(connection => console.log(`Connection Established to db`))
+//   .catch(connectionError => console.log("Connection Failed!", connectionError));
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL);
 } else {
   mongoose.connect("mongodb://localhost/breaddit123");
 }
+
+mongoose.Promise = Promise;
 
 module.exports = mongoose;
