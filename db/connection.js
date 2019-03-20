655bcc22bae742393c44267c88970317d5af59fb
Joey Deras <jmderas@yahoo.com>
2019-03-20 15:08:10 -0400

    heroku is deployed

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,5 @@
 //This is the DB connection it will include how our database and Express.js will be connected using Mongoose.
 const mongoose = require("../models/Post");
-// const mongoUri = "mongodb://localhost/Breaddit";
-
-// mongoose
-//   .connect(mongoUri)
-//   .then(connection => console.log(`Connection Established to db`))
-//   .catch(connectionError => console.log("Connection Failed!", connectionError));
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
