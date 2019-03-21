59c156b9862c62049dceb4d82ec236b29d9cae05
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-03-21 13:21:31 -0400

    meets minimum backend MVP req

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,12 @@
 //This is the main model for the Breaddit clone. It will have full CRUD, but its only components are going to be a title and a body. KISS for the win.
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Comment = require('./Comment');
 
 const Post = new Schema({
 	title: String,
-	body: String
+	body: String,
+	comments: [ Comment ]
 });
 
 mongoose.model('Post', Post);
