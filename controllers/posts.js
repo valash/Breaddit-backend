cc6dc4a5da46463e0fbe145beeb5395614e29898
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-03-21 09:52:25 -0400

    getting rid of this branch

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,21 @@
 //This is the controller for the post. It will most likely also include the commands for the comments since the comments will be a sub model. It' going to be using router b/c we have to do that, it'll be connected to the Post model and the connection file, so it can, you know access the dadtabase and actually use the model at hand, since those things are kinda important. Then it'll have a whole bunch of routes to places so it can CRUD it up. Bars.
 
+// Zakk's Notes:
+// Define a set of routes for Post
+//   - /post
+
+// Define a set of nested routes for Comments
+//  - /post/:postId/comment
+// GET /post/:postId/comment (return a single comment) *probably don't care about this one
+// POST /post/:postId/comment (create a new post)
+//		- get post by ID and push comment into Post model comments array
+// PUT /post/:postId/comment/:commentId (update a given post by ID)
+//		- find comment within post (by ID), update it, save it
+// DELETE /post/:postId/comment/:commentId (delete a post)
+//		- use $pull to remove it from the comments array (like POST/$push)
+
 const express = require('express');
 const postRouter = express.Router();
-const commentRouter = express.Router({ mergeParams: true });
 
 postRouter.use('/:id/comments', commentRouter);
 
@@ -44,6 +57,11 @@ postRouter.put('/:_id', (req, res) => {
 	});
 });
 
+postRouter.post('/:id/comment', (req, res) => {
+	// findOneAndUpdate post by ID,
+	// $push comment into post
+});
+
 //reads all comments
 commentRouter.get('/:_id/comments', (req, res) => {
 	Post.find({ _id: req.params._id }, { comments }).then((comments) => {
@@ -52,17 +70,10 @@ commentRouter.get('/:_id/comments', (req, res) => {
 });
 
 //deletes comments
-<<<<<<< HEAD
-router.delete('/:postId/comments/:commentId', (req, res) => {
-	Post.findOne({ _id: req.params.postId }).then((post) => {
-		Comment.findOneAndDelete({ _id: req.params.commentId }).then((post) => {
-			res.json(post);
-		});
-=======
+
 commentRouter.put('/:_id/comments', (req, res) => {
 	Post.comments.id(_id).remove().then(res.redirect('/:_id')).then((post) => {
 		res.json(post);
->>>>>>> bd3a462e07b11f6d6f7027500abc70ef1d02188e
 	});
 });
 
